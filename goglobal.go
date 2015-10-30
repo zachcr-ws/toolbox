ab41a6406ddec08a027ce276d2e05596a794d2d1
ZachBergh <zach@aeris.io>
2015-10-30 19:56:08 +0800

    gobal

---
+++ goglobal.go
@@ -0,0 +1,40 @@
+package common
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+// A general gob encoder
+// Example:
+// byteData, err = GobGeneralEncoder(input)
+func GobGeneralEncoder(input interface{}) ([]byte, error) {
+	buffer := bytes.NewBuffer([]byte{})
+	enc := gob.NewEncoder(buffer)
+	err := enc.Encode(input)
+	if err != nil {
+		return nil, err
+	}
+	return buffer.Bytes(), nil
+}
+
+// A general gob decoder
+// Example:
+// 1) base type
+// var baseVar string
+// err := GobGeneralDecoder(byteData, "", &baseVar)
+//
+// 2) custom struct
+// var youVar yourStruct
+// err := GobGeneralDecoder(byteData, yourStruct{}, &youVar)
+func GobGeneralDecoder(input []byte, structType interface{}, value interface{}) error {
+	buffer := bytes.NewBuffer(input)
+	gob.Register(structType)
+
+	dec := gob.NewDecoder(buffer)
+	err := dec.Decode(value)
+	if err != nil {
+		return err
+	}
+	return nil
+}
