f3e9e45c916ea61579e9593918bcf99f146335ef
ZachBergh <zach@aeris.io>
2016-08-01 17:41:07 +0800

    random str

--- mongo/utils.go
+++ mongo/utils.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"gopkg.in/mgo.v2"
+	"math/rand"
 	"strconv"
 	"strings"
 	"time"
@@ -15,6 +16,17 @@ func MgoGeneratorId(r string) string {
 	ctx := md5.New()
 	st := strconv.FormatInt(time.Now().UnixNano(), 16)
 	return hex.EncodeToString(ctx.Sum([]byte(st + r)))
+}
+
+func MgoGeneratorRandom(leng int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < leng; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
 }
 
 func GetSession(hosts string, user string, password string, db string) (*mgo.Session, error) {
